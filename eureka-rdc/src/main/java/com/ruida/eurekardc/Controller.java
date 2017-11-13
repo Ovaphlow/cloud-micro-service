30f84dcc8cb931b47e6877d5a72e9ce76170fde5
ovaphlow <jsfreecss@163.com>
2017-11-13 10:19:30 +0800

    .

---
+++ eureka-rdc/src/main/java/com/ruida/eurekardc/Controller.java
@@ -0,0 +1,32 @@
+package com.ruida.eurekardc;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/test/")
+    public String testi() {
+        return "response from eureka-controller-0";
+    }
+
+    @RequestMapping("/test/{{fallback}}")
+    @HystrixCommand(fallbackMethod = "helloFallbackMethod")
+    public String test(
+            @PathVariable("fallback") String fallback
+    ) {
+        System.out.println("test");
+        if ("1".equals(fallback)) {
+            throw new RuntimeException(",,;;=ij12e");
+        }
+        return "test test hello world";
+    }
+
+    public String helloFallbackMethod(String fallback) {
+        return "fallback 参数值为 " + fallback;
+    }
+}
---
+++ eureka-rdc/src/test/java/com/ruida/eurekardc/EurekaRdcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ruida.eurekardc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaRdcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
